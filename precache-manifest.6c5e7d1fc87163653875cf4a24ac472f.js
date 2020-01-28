a963015e2748c5b541d05f9aac1b7867d3d5755c
kai <kmirlan88@gmail.com>
2020-01-27 19:20:18 -0600

    Updates

---
+++ precache-manifest.6c5e7d1fc87163653875cf4a24ac472f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e27cdf62a524d7005abcb19073c03df",
+    "url": "/freeStuff/index.html"
+  },
+  {
+    "revision": "213a0caf04bf6c14ab81",
+    "url": "/freeStuff/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5be52085a765ff35ee3b",
+    "url": "/freeStuff/static/js/2.2f7f7d40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/freeStuff/static/js/2.2f7f7d40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "213a0caf04bf6c14ab81",
+    "url": "/freeStuff/static/js/main.e1d7341f.chunk.js"
+  },
+  {
+    "revision": "d17f4ee9afa6f29cab2d",
+    "url": "/freeStuff/static/js/runtime-main.0b9ed95a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/freeStuff/static/media/logo.5d5d9eef.svg"
+  }
+]);
