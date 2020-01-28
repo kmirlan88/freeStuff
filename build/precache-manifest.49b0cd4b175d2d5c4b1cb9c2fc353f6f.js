9a732cfef4ad9f52f171403916ea4be1fe3e002a
kai <kmirlan88@gmail.com>
2020-01-27 19:59:24 -0600

    new build

---
+++ build/precache-manifest.49b0cd4b175d2d5c4b1cb9c2fc353f6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fa96b06ecfe0799abae9fe00ee7337",
+    "url": "/freeStuff/index.html"
+  },
+  {
+    "revision": "90a57577573644b32561",
+    "url": "/freeStuff/static/css/main.fe5c1fab.chunk.css"
+  },
+  {
+    "revision": "1a3f76182905cd0c691f",
+    "url": "/freeStuff/static/js/2.f70d6912.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/freeStuff/static/js/2.f70d6912.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90a57577573644b32561",
+    "url": "/freeStuff/static/js/main.bc43223c.chunk.js"
+  },
+  {
+    "revision": "38f06b6a9ebb461257bd",
+    "url": "/freeStuff/static/js/runtime-main.2672783a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/freeStuff/static/media/logo.5d5d9eef.svg"
+  }
+]);
