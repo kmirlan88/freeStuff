00f8d33ab74ea5ebe31a6da9b447877f524157f6
kai <kmirlan88@gmail.com>
2020-01-27 21:51:00 -0600

    movie

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar'
+import Movie from './Movie'
 
 function App() {
   return (
     <div className="App">
-      <div class="ui secondary  menu">
-          <a class="item active">
-            Home
-          </a>
-          <a class="item">
-            Get Free Stuff
-          </a>
-          <a class="item">
-            Post Free Stuff
-          </a>
-          <div class="right menu">
-            <div class="item">
-              <div class="ui icon input">
-                <input type="text" placeholder="Search..." />
-                <i class="search link icon"></i>
-              </div>
-            </div>
-            <a class="ui item">
-              Logout
-            </a>
-          </div>
-      </div>
+      <Navbar />
+      <Movie />
     </div>
   );
 }
