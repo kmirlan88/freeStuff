13bd9b78042502d249a7ecf7f4a9e5c4954f8beb
kai <kmirlan88@gmail.com>
2020-02-10 21:27:28 -0600

    get input

--- src/Form.js
+++ src/Form.js
@@ -1,40 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 
-
-const getInputData = event => {
-	event.preventDefault();
-	console.log(event.target);
-	// performSearch(event.target.value);
-};
 
 function Form() {
+	const [state, setState] = React.useState({
+		itemName: "",
+		street: "",
+		state: "",
+		country: "",
+		img: "",
+		descr: ""
+	});
+
+	function handleChange(evt) {
+		const value = evt.target.value;
+		setState({
+			...state,
+			[evt.target.name]: value
+		});
+	}
+
+	const getInputData = event => {
+		event.preventDefault();
+		console.log(state);
+		// performSearch(event.target.value);
+	};
 	return (
 		<form onSubmit={getInputData}>
 			<div className="ui form">
 				<div className="field">
 					<label>Item Name</label>
-					<input type="text" name="first-name" placeholder="Item Name" />
+					<input
+						type="text"
+						name="itemName"
+						placeholder="Item Name"
+						value={state.itemName}
+						onChange={handleChange}
+					/>
 				</div>
 				<div className="three fields">
 					<div className="field">
 						<label>Street Address</label>
-						<input type="text" placeholder="Street Address" />
+						<input
+							type="text"
+							placeholder="Street Address"
+							name="street"
+							value={state.street}
+							onChange={handleChange}
+						/>
 					</div>
 					<div className="field">
 						<label>State</label>
-						<input type="text" placeholder="State" />
+						<input
+							type="text"
+							placeholder="State"
+							name="state"
+							value={state.state}
+							onChange={handleChange}
+						/>
 					</div>
 					<div className="field">
 						<label>Country</label>
-						<input type="text" placeholder="Country" />
+						<input
+							type="text"
+							placeholder="Country"
+							name="country"
+							value={state.country}
+							onChange={handleChange}
+						/>
 					</div>
 				</div>
 				<img
 					className="ui small rounded image"
-					src="https://semantic-ui.com/images/wireframe/square-image.png"></img>
+					src="https://semantic-ui.com/images/wireframe/square-image.png"
+					name="img"
+					value={state.img}
+					onChange={handleChange}></img>
 				<div className="field">
 					<label>Descripton:</label>
-					<textarea></textarea>
+					<textarea
+						name="descr"
+						value={state.descr}
+						onChange={handleChange}></textarea>
 				</div>
 				<button className="ui primary button" type="submit">
 					Post
